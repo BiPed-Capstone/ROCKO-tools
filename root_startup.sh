9a93f7e8c516b5105005e6e6107e8820b839cddd
elizabeth <elizabeth@ziemer.net>
2025-01-28 18:16:49 -0500

    New venv dir is home

--- root_startup.sh
+++ root_startup.sh
@@ -1,5 +1,5 @@
 source /opt/ros/jazzy/setup.bash 
-source /home/rocko/ROCKO-env/.venv/bin/activate
+source /home/rocko/.venv/bin/activate
 source /home/rocko/ROCKO-env/install/setup.bash
 cd /home/rocko/ROCKO-env
 ros2 launch rocko_env rocko.launch.py
