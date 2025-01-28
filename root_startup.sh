6115f2e111a5429db748ec7f4d1dbecfef6647a1
elizabeth <elizabeth@ziemer.net>
2025-01-28 18:15:57 -0500

    Fixes to path specification

--- root_startup.sh
+++ root_startup.sh
@@ -1,4 +1,5 @@
 source /opt/ros/jazzy/setup.bash 
-source /home/ROCKO-env/.venv/bin/activate
-source /home/ROCKO-env/install/setup.bash
+source /home/rocko/ROCKO-env/.venv/bin/activate
+source /home/rocko/ROCKO-env/install/setup.bash
+cd /home/rocko/ROCKO-env
 ros2 launch rocko_env rocko.launch.py
