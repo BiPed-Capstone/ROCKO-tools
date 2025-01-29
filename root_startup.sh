a5d5421181b125e2ea9da251c9903143addc147c
elizabeth <elizabeth@ziemer.net>
2025-01-29 11:54:08 -0500

    Assemble ROS instances cross-platform

--- root_startup.sh
+++ root_startup.sh
@@ -1,5 +1,6 @@
 source /opt/ros/jazzy/setup.bash 
 source /home/rocko/.venv/bin/activate
 source /home/rocko/ROCKO-env/install/setup.bash
+export ROS_DOMAIN_ID=1
 cd /home/rocko/ROCKO-env
 ros2 launch rocko_env rocko.launch.py
