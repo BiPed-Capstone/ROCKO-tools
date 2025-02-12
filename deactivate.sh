673b8b70a91e3fc8c1ad2b01c7b8a0d1af27b171
elizabeth <elizabeth@ziemer.net>
2025-02-12 11:46:43 -0500

    Updated to invoke environment variables

--- deactivate.sh
+++ deactivate.sh
@@ -1,1 +1,5 @@
+source /opt/ros/jazzy/setup.bash 
+source /home/rocko/.venv/bin/activate
+source /home/rocko/ROCKO-env/install/setup.bash
+export ROS_DOMAIN_ID=17
 ros2 control switch_controllers --deactivate left_velocity_pid_controller left_balancing_pid_controller right_balancing_pid_controller right_velocity_pid_controller
