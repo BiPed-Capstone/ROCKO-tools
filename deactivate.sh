b7221dc9c97f97b8c3fdba3bf751132e2f436f6a
Zoe Edgington <edgingza@rose-hulman.edu>
2025-02-13 14:12:48 -0500

    Added hardware deactivation

--- deactivate.sh
+++ deactivate.sh
@@ -2,4 +2,6 @@ source /opt/ros/jazzy/setup.bash
 source /home/rocko/.venv/bin/activate
 source /home/rocko/ROCKO-env/install/setup.bash
 export ROS_DOMAIN_ID=17
+ros2 control set_hardware_component_state RightDriveMotor inactive
+ros2 control set_hardware_component_state LeftDriveMotor inactive
 ros2 control switch_controllers --deactivate left_velocity_pid_controller left_balancing_pid_controller right_balancing_pid_controller right_velocity_pid_controller
